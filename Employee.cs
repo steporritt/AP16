8759e270430e4dfcad8bef20335de42b06b9f99d
Stephen Porritt <StephenPorritt@hotmail.co.uk>
2020-06-10 23:46:55 +0100

    Set up project and created employee class.
    Started work on loading employees.

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AP16
+{
+    class Employee
+    {
+        // In this instance I've chosen to store the Id as a string because I don't know the possible values for Id
+        // e.g. it is not unreasonable that employee Ids may start with a '0'.
+        public string Id { get; set; }
+
+        // I would consider what the additional properties were and if they were required.
+        // For the purposes of this exercise, I have chosen to store the remainder of the employee line
+        // as a single string
+        public string AdditionalInformation { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AP16
 {
@@ -6,7 +9,30 @@ namespace AP16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // We could replace these with parameters to the application in future
+            var sourceFileLocation = "Resources\\Employees.txt";
+            var recordsToDeleteFileLocation = "Resources\\EmployeesToDelete.txt";
+
+
         }
+
+        /// <summary>
+        /// Reads a list of employees from a file and returns them as a List of Employee
+        /// </summary>
+        /// <param name="InputFilePath">The source file containing the list of employees</param>
+        /// <returns>A List of Employee</returns>
+        private static IEnumerable<Employee> LoadEmployees(string InputFilePath)
+        {
+            if (!File.Exists(InputFilePath))
+                throw new FileNotFoundException($"LoadEmployees() could not find the specified file: \"{InputFilePath}\".");
+            
+            var employees = File.ReadAllLines(InputFilePath);
+
+            return employees.ToList();
+
+            return employees;
+        }
+
+        private
     }
 }
